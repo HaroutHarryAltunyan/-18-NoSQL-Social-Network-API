73c5b152a6a457fb7d7b3864276a76afea326d4d
Haroutyun Harry Altunyan <haroutyunhaltunyan93@icloud.com>
2025-01-20 18:39:52 -0800

    routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -9,8 +9,11 @@ const {
   removeReaction,
 } = require('../../controllers/thoughtController');
 
+// Routes for getting all thoughts and creating a new thought
 router.route('/').get(getThoughts).post(createThought);
+// Routes for operations on a single thought by ID
 router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+// Routes for adding and removing reactions
 router.route('/:thoughtId/reactions').post(addReaction).delete(removeReaction);
 
 module.exports = router;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -9,8 +9,11 @@ const {
   removeFriend,
 } = require('../../controllers/userController');
 
+// Routes for getting all users and creating a new user
 router.route('/').get(getUsers).post(createUser);
+// Routes for operations on a single user by ID
 router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+// Routes for adding and removing friends
 router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
 
 module.exports = router;
